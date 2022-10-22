5092ae0e7d64cc5c90b992d1f3177bb3140b3eb8
Iman Ali <60799230+iman20004@users.noreply.github.com>
2022-10-21 20:54:32 -0400

    react app and server created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
-// backend starting point
+// THESE ARE NODE APIs WE WISH TO USE
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
+
+// CREATE OUR SERVER
+dotenv.config()
+const PORT = process.env.PORT || 4000;
+const app = express()
+
+// SETUP THE MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+// SETUP OUR OWN ROUTERS AS MIDDLEWARE
+const piecesRouter = require('./routes/pieces-router')
+app.use('/api', piecesRouter)
+
+// CONNECT TO DATABASE 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true , useUnifiedTopology: true})
+    .then(() => {
+        app.listen({ port: PORT }, () => {
+            console.log(`Server ready at http://localhost:${PORT}`);
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    });
---
+++ server/routes/pieces-router.js
@@ -0,0 +1,8 @@
+//const auth = require('../auth')
+const express = require('express')
+
+
+const router = express.Router()
+
+
+module.exports = router
