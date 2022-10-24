0fc9b004d437f56da7c96875526172191a7b7ad2
PGreatness <ahnafhasan2000@gmail.com>
2022-10-24 18:22:34 -0400

    help

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 const config = require("config");
 
 // CREATE OUR SERVER
-const PORT = process.env.PORT || 5000;
+const PORT = config.get('port') || 5000;
 const serverDomain = "https://pieces-316.herokuapp.com";
 const app = express()
 
