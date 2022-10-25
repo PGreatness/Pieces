7c75e71bf1be3ebcc2f2d150b79e0ab8993c3250
PGreatness <ahnafhasan2000@gmail.com>
2022-10-24 20:31:23 -0400

    help

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require("config");
 
 // CREATE OUR SERVER
 const PORT = config.get('port') || 5000;
-const serverDomain = "https://pieces-316.herokuapp.com";
+const serverDomain = "localhost:4000";
 const app = express()
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
@@ -16,10 +16,7 @@ app.use('/api', piecesRouter)
 
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: ["http://pieces-316.herokuapp.com", "https://pieces-316.herokuapp.com", config.get("server_local_domain")],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
@@ -30,9 +27,9 @@ app.use('/test', (req, res) => {
 // CONNECT TO DATABASE
 mongoose.connect(config.get("mongo_uri"), {useNewUrlParser: true , useUnifiedTopology: true})
     .then(() => {
-        require('http').createServer(app).listen(PORT, () => {
+        app.listen({ port: PORT }, () => {
             console.log(`Server is running on port ${PORT}`)
-        })
+        });
     })
     .catch(error => {
         console.log(error)
