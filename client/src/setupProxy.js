0d9486482ad306291f2669fec2e7d84d9fdddf72
Iman Ali <60799230+iman20004@users.noreply.github.com>
2022-10-24 22:31:16 -0400

    still trying

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,5 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
-module.exports = function (app) {
-    app.use(createProxyMiddleware('/api/**', { target: 'http://localhost:4000' }));
-    //app.use(createProxyMiddleware('/otherApi/**', { target: 'http://localhost:4000' }));
+module.exports = app => {
+  app.use(proxy("/api/*", { target: "http://localhost:4000/" }));
 };
