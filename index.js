a1ab3d68a764b60cc5da88dc80571c2cc911ec34
Iman Ali <60799230+iman20004@users.noreply.github.com>
2022-10-25 00:59:14 -0400

    prod

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const cookieParser = require('cookie-parser')
 const config = require("config");
 
 // CREATE OUR SERVER
-const PORT = config.get("port") || 4000;
+const PORT = process.env.PORT || 4000;
 const serverDomain = "localhost";
 const app = express()
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "/client/build", "index.html"));
+  });
+}
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
 const piecesRouter = require('./routes/pieces-router')
@@ -19,13 +26,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
 
 // CONNECT TO DATABASE
 mongoose.connect(config.get("mongo_uri"), {useNewUrlParser: true , useUnifiedTopology: true})
