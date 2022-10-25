fe5369773f8f0af39567929a5fd886667752f5de
Tommy <tommylin121314@gmail.com>
2022-10-25 13:59:03 -0400

    Made changes to the map tests

--- tests/map.test.js
+++ tests/map.test.js
@@ -18,7 +18,7 @@ let ownerIdOfDeleted = null
 //     await mongoose.connection.close();
 // });
 
-// 
+// ADDING MAP SUCCESS
 describe("POST /api/map/newMap", () => {
     it("Should add a Map to the database", async () => {
         const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
@@ -39,6 +39,115 @@ describe("POST /api/map/newMap", () => {
     });
 });
 
+// ADDING MAP WITH SAME NAME
+describe("POST /api/map/newMap", () => {
+    it("Can't add Map with name already taken", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
+            "mapName": mapName,
+            "mapDescription": "This map will be deleted vert shortly!",
+            "tags": ["188", "Scary"],
+            "mapBackgroundColor": "#188188",
+            "mapHeight": 1024,
+            "mapWidth": 1024,
+            "tileHeight": 64,
+            "tileWidth": 64,
+            "ownerId": "6355e171286afe702190fe10"
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Another Map owned by the same User already has this name");
+    });
+});
+
+// ADDING MAP WITH NEGATIVE HEIGHT
+describe("POST /api/map/newMap", () => {
+    it("Can't have negative height", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
+            "mapName": "nojwoqjwojqowqjwqo",
+            "mapDescription": "This map will be deleted vert shortly!",
+            "tags": ["188", "Scary"],
+            "mapBackgroundColor": "#188188",
+            "mapHeight": -1024,
+            "mapWidth": 1024,
+            "tileHeight": 64,
+            "tileWidth": 64,
+            "ownerId": "6355e171286afe702190fe10"
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Map can not have a height of zero or less pixels.");
+    });
+});
+
+// ADDING MAP WITH NEGATIVE WIDTH
+describe("POST /api/map/newMap", () => {
+    it("Can't have negative width", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
+            "mapName": "nojwoqjwojqowqjwqo",
+            "mapDescription": "This map will be deleted vert shortly!",
+            "tags": ["188", "Scary"],
+            "mapBackgroundColor": "#188188",
+            "mapHeight": 1024,
+            "mapWidth": -1024,
+            "tileHeight": 64,
+            "tileWidth": 64,
+            "ownerId": "6355e171286afe702190fe10"
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Map can not have a width of zero or less pixels.");
+    });
+});
+
+// ADDING MAP WITH NEGATIVE TILE HEIGHT
+describe("POST /api/map/newMap", () => {
+    it("Can't have negative tile height", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
+            "mapName": "nojwoqjwojqowqjwqo",
+            "mapDescription": "This map will be deleted vert shortly!",
+            "tags": ["188", "Scary"],
+            "mapBackgroundColor": "#188188",
+            "mapHeight": 1024,
+            "mapWidth": 1024,
+            "tileHeight": -64,
+            "tileWidth": 64,
+            "ownerId": "6355e171286afe702190fe10"
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Map can not have a height of zero or less tiles.");
+    });
+});
+
+// ADDING MAP WITH NEGATIVE TILE WIDTH
+describe("POST /api/map/newMap", () => {
+    it("Can't have negative tile width", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/newMap").send({
+            "mapName": "nojwoqjwojqowqjwqo",
+            "mapDescription": "This map will be deleted vert shortly!",
+            "tags": ["188", "Scary"],
+            "mapBackgroundColor": "#188188",
+            "mapHeight": 1024,
+            "mapWidth": 1024,
+            "tileHeight": 64,
+            "tileWidth": -64,
+            "ownerId": "6355e171286afe702190fe10"
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Map can not have a width of zero or less tiles.");
+    });
+});
+
+// DELETING MAP SUCCESS
+describe("POST /api/map/deleteMap", () => {
+    console.log("POST /api/map/deleteMap")
+    it("Should delete a Map from the database", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/map/deleteMap").query({
+            "id": objectIdOfDeleted,
+            "ownerId": "6355e171286afe702190fe20"
+        })
+        expect(res.status).toBe(401);
+        expect(res.body.message).toBe("User does not have ownership of this Map");
+    });
+});
+
+// DELETING MAP SUCCESS
 describe("POST /api/map/deleteMap", () => {
     console.log("POST /api/map/deleteMap")
     it("Should delete a Map from the database", async () => {
