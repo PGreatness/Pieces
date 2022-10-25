321d168d2e5637d409615ab039ad2ee320c58f0f
vchiSBU <vincent.chi@stonybrook.edu>
2022-10-25 16:29:23 -0400

    Initial tileset test (needs debug)

---
+++ tests/tileset.test.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+
+require("dotenv").config();
+
+let tilesetName = "testTilesetJest"
+let objectIdOfDeleted = null
+let ownerIdOfDeleted = null
+
+describe("POST /api/tileset/newTileset", () => {
+    it("Should add a Tileset to the database", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/tileset/newTileset").send({
+            "tilesetName": tilesetName,
+            "imagePixelHeight": 64,
+            "imagePixelWidth": 64,
+            "tileHeight": 8,
+            "tileWidth": 8,
+            "source": "testSource",
+            "ownerId": "635364191c83e20194521f1e",
+            "isPublic": false,
+            "isLocked": false
+        });
+        expect(res.status).toBe(200);
+        console.log(res.body)
+        expect(res.body.tileset.tilesetName).toBe(tilesetName);
+        expect(res.body.message).toBe("A tileset has been created!")
+        objectIdOfDeleted = res.body.id.toString();
+        ownerIdOfDeleted = res.body.tileset.ownerId;
+    });
+});
+
+describe("POST /api/tileset/newTileset", () => {
+    it("Should fail to add a Tileset of the same name to the database", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/tileset/newTileset").send({
+            "tilesetName": tilesetName,
+            "imagePixelHeight": 64,
+            "imagePixelWidth": 64,
+            "tileHeight": 8,
+            "tileWidth": 8,
+            "source": "testSource",
+            "ownerId": "635364191c83e20194521f1e",
+            "isPublic": false,
+            "isLocked": false
+        });
+        expect(res.status).toBe(400);
+        expect(res.body.errorMessage).toBe("Another Tileset owned by the same User already has this name.")
+    });
+});
+
+describe("POST /api/tileset/updateTileset", () => {
+    it("Should update Tileset imagePixelHeight field", async () => {
+        const res = await request("http://pieces-316.herokuapp.com").post("/api/tileset/updateTileset").send({
+            "imagePixelHeight": 128,
+        }).query({
+            "id": objectIdOfDeleted,
+            "ownerId": ownerIdOfDeleted
+        });
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe("Tileset was successfully updated")
+    })
+})
