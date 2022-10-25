7154911629eda37035c159ef5fc3f6333f3b7066
PGreatness <ahnafhasan2000@gmail.com>
2022-10-25 05:51:41 -0400

    extra _id field"

--- models/tile-model.js
+++ models/tile-model.js
@@ -1,10 +1,6 @@
 const { model, Schema, ObjectId } = require("mongoose");
 
 const tileSchema = new Schema({
-    _id: {
-        type: ObjectId,
-        required: true,
-    },
     tilesetId: {
         type: ObjectId,
         required: true,
