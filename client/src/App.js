dac0eb0f4208a34dac0e10b5dfb7891ba5a8d448
Iman Ali <60799230+iman20004@users.noreply.github.com>
2022-10-28 18:56:30 -0400

    fixing routes

--- client/src/App.js
+++ client/src/App.js
@@ -15,25 +15,27 @@ const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<div className="Loading">Loading...</div>}>
+        <div>
         <Navbar/>
         <Routes>
-          <Route path="/" exact component={WelcomeScreen}/>
-          <Route path="/profile/" exact component={ProfileScreen}/>
-          <Route path="/explore/" exact component={ExploreScreen}/>
-          <Route path="/library/" exact component={LibraryScreen}/>
-          <Route path="/community/" exact component={CommunityScreen}/>
+          <Route path="/" element={<WelcomeScreen/>}/>
+          <Route path="/profile/" element={<ProfileScreen/>}/>
+          <Route path="/explore/" element={<ExploreScreen/>}/>
+          <Route path="/library/" element={<LibraryScreen/>}/>
+          <Route path="/community/" element={<CommunityScreen/>}/>
 
           <Route
             path="/tileset/:id"
-            exact component={TilesetEditor}
+            element={<TilesetEditor/>}
           />
           <Route
             path="/map/:id"
-            exact component={MapEditor}
+            element={<MapEditor/>}
           />
 
-          <Route path="/reset-password/:id/:token" exact component={ResetPasswordScreen}/>
+          <Route path="/reset-password/:id/:token" element={<ResetPasswordScreen/>}/>
         </Routes>
+        </div>
       </Suspense>
     </BrowserRouter>
   );
--- client/src/components/WelcomeScreen.js
+++ client/src/components/WelcomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function WelcomeScreen() {
+    console.log("why not bro")
     return (
         <div>Welcome Screen</div>
     )
