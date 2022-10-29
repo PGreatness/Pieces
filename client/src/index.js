99ed60cfa65770d08b41493e7ca8a62741587e68
PGreatness <ahnafhasan2000@gmail.com>
2022-10-28 22:15:06 -0400

    1

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+console.log("1")
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("2")
 root.render(
   <React.StrictMode>
     <App />
