2cdb148f9818fc24f0e2d4d5471e8545c62c26c7
PGreatness <ahnafhasan2000@gmail.com>
2022-11-01 18:59:10 -0400

    remove extra comment

--- client/src/components/Screens/Explore/ExploreScreen.js
+++ client/src/components/Screens/Explore/ExploreScreen.js
@@ -4,7 +4,6 @@ import Explore from './Explore'
 import ExploreComments from './ExploreComments'
 import { useNavigate } from 'react-router-dom';
 
-
 export default function ExploreScreen(props) {
     const navigate = useNavigate();
     const [showComments, setShowComments] = React.useState(false);
@@ -12,7 +11,6 @@ export default function ExploreScreen(props) {
     return (
         <Box style={{height: '100%', width: '80vw', color: 'white', backgroundColor: '#1F293A',
          display:'flex', zIndex:0, position: 'relative'}}>
-            {/* <SocialSidebar></SocialSidebar> */}
             {!showComments? (<Explore setLoc={props.setLoc}
                 setShowComments={() => {
                     setShowComments(true);
