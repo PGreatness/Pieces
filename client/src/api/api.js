82cc10b66cd98bb71d252d87ceade5b5630a7feb
Iman Ali <60799230+iman20004@users.noreply.github.com>
2022-11-05 16:18:30 -0400

    calling backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    //baseURL: 'http://localhost:4000/api',
+    baseURL: 'http://pieces-316.herokuapp.com/api',
 })
 
 
