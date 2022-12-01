6258de79ea45b889dde181da852a77d041322e4c
PGreatness <ahnafhasan2000@gmail.com>
2022-11-30 19:52:10 -0500

    fix onclick

--- client/src/components/Screens/Library/LibraryScreen.js
+++ client/src/components/Screens/Library/LibraryScreen.js
@@ -294,9 +294,14 @@ export default function LibraryScreen(props) {
     }
 
     const openProject = (project) => {
-        setLocation('/map/'+project._id);
         console.log(project);
-        store.changePageToMapEditor(project)
+        if (!project.tilesetDesc) {
+            setLocation('/map/'+project._id);
+            store.changePageToMapEditor(project);
+        }else{
+            setLocation('/tileset/'+project._id);
+            store.changePageToTilesetEditor(project);
+        }
     }
 
     return (
