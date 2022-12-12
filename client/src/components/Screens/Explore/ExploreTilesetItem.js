d6212160d64fa94fba6faac410bf4221d36e9d74
vchiSBU <vincent.chi@stonybrook.edu>
2022-12-12 00:23:33 -0500

    Same overflow fix for descriptions

--- client/src/components/Screens/Explore/ExploreTilesetItem.js
+++ client/src/components/Screens/Explore/ExploreTilesetItem.js
@@ -211,7 +211,7 @@ export default function ExploreTilesetItem(props) {
                 <Box style={{ display: 'flex', flexDirection: 'row' }} >
                     <Box style={{ width: '60%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }} >
                         <Typography style={{ marginLeft: '20px', fontSize: '40px', fontWeight: '700' }} color='azure'>{project.title.length > 25 ? project.title.substring(0, 24) + "..." : project.title}</Typography>
-                        <Typography style={{ marginLeft: '20px', fontSize: '20px', fontWeight: '300', paddingBottom: '10px' }} color='azure'>{project.tilesetDesc}</Typography>
+                        <Typography style={{ marginLeft: '20px', fontSize: '20px', fontWeight: '300', paddingBottom: '10px' }} color='azure'>{project.tilesetDesc.length > 25 ? project.tilesetDesc.substring(0, 24) + "..." : project.tilesetDesc}</Typography>
                     </Box>
                     <Box style={{ width: '40%', padding: '20px', display: 'flex', alignItems: 'center', justifyContent: 'end', flexDirection: 'row' }} >
                         <Box style={{ display: 'flex', flexDirection: 'column' }}>
